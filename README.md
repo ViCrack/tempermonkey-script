3eca9bcf44486873e4a5ed22ea85bc10d152e3e4
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-02-20 01:36:41 +0800

    统一反馈地址

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 用于一些网站优化的 tempermonkey 脚本
 
+[https://greasyfork.org/zh-CN/users/306433](https://greasyfork.org/zh-CN/users/306433)
+
 ## 自动展开全文阅读更多
 
-自动展开网站内容而无需点击，去广告，去掉需要打开app的提示
+自动展开网站内容而无需点击，去广告，去掉需要打开 app 的提示
 
+## Flash iframe allowfullscreen
+
+解决某些网页 flash 播放器点击全屏无效的问题 (请求全屏已拒绝，至少一个文档包含的元素不是一个 iframe，或没有"allowfullscreen"属性。 index TypeError: The expression cannot be converted to return the specified type.)
+
+## 去除TAPD水印
+
+去除tapd在页面中显示的用户名水印
+
+## 联想知识库无需扫码关注也可继续浏览内容
+
+...
---
+++ 去除TAPD水印.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         去除TAPD水印
+// @namespace    https://greasyfork.org/zh-CN/users/306433
+// @version      0.2
+// @description  去除tapd在页面中显示的用户名水印
+// @author       https://greasyfork.org/zh-CN/users/306433-baster
+// @supportURL   https://greasyfork.org/zh-CN/users/306433-baster
+// @homepageURL  https://greasyfork.org/zh-CN/users/306433
+// @match        https://www.tapd.cn/*
+// @icon         https://www.tapd.cn/favicon.ico
+// @license      GNU
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+  'use strict'
+
+  var element = document.createElement('style')
+  element.setAttribute('type', 'text/css')
+  document.head.appendChild(element)
+  element.textContent = '.dontdeleteme-div {background-size: 0px !important;} '
+
+  /* MutationObserver delete+change_style
+        .dontdeleteme-div {
+    		width: 100% !important;
+    		height: 100% !important;
+    		max-width: unset !important;
+    		max-height: unset !important;
+    		position: absolute !important;
+    		top: 0 !important;
+    		left: 0 !important;
+    		padding: 0 !important;
+    		margin: 0 !important;
+    		z-index: 1001 !important;
+    		pointer-events: none !important;
+    		background-repeat: repeat !important;
+    		background-image: url('data:image/svg+xml;base64,xxxx') !important;
+    		opacity: 0.04 !important;
+    		display: block !important;
+    		visibility: visible !important;
+    		clip: initial !important;
+    		clip-path: initial !important;
+    		transform: initial !important;
+    	}
+    	*/
+})()
