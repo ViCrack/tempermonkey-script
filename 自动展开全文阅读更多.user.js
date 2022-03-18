1f0b03c372d71233d03197fc89012b9d531e9fae
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-03-18 21:53:53 +0800

    暂停时候中间区域隐藏

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.57.2
+// @version     1.58.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十多个网站，前期脚本更新可能会比较频繁】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -68,6 +68,7 @@
 // @match       *://m.meishichina.com/*
 // @match       *://www.logonews.cn/*
 // @match       *://cn.bing.com/search?q=*
+// @match       *://segmentfault.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -76,6 +77,17 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://segmentfault.com/*"],
+            directLink: [
+                "*://link.segmentfault.com/?enc=*",
+                (node) => {
+                    if (/^https?:\/\//.test(node.innerText)) {
+                        node.href = node.innerText;
+                    }
+                },
+            ],
+        },
         {
             match: ["*://cn.bing.com/search?q=*"],
             bindClick: [
@@ -133,7 +145,7 @@
             directLink: [
                 "*://jump*.bdimg.com/safecheck/index?url=*",
                 (node) => {
-                    if (/^https?:/.test(node.innerText)) {
+                    if (/^https?:\/\//.test(node.innerText)) {
                         node.href = node.innerText;
                     }
                 },
