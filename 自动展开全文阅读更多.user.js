5d52cafbbb79b2640b4b62a0e1b62d6d0cf7b767
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-03-19 17:45:05 +0800

    ld246.com 链滴

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.58.0
+// @version     1.59.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十多个网站，前期脚本更新可能会比较频繁】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -69,6 +69,7 @@
 // @match       *://www.logonews.cn/*
 // @match       *://cn.bing.com/search?q=*
 // @match       *://segmentfault.com/*
+// @match       *://ld246.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -77,6 +78,10 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://ld246.com/*"],
+            directLink: ["*://link.ld246.com/forward?goto=*", "goto"],
+        },
         {
             match: ["*://segmentfault.com/*"],
             directLink: [
