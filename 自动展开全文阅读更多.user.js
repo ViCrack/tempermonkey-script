67e286729737977c207e5d174c65482fdca8b512
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-03-23 00:05:44 +0800

    tt

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.60.0
+// @version     1.61.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十多个网站，前期脚本更新可能会比较频繁】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -71,6 +71,7 @@
 // @match       *://segmentfault.com/*
 // @match       *://ld246.com/*
 // @match       *://www.bilibili.com/read/mobile?id=*
+// @match       *://*.toutiao.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -79,6 +80,12 @@
 
 (function () {
     var websites = [
+        {
+            // https://wukong.toutiao.com/question/6712757183118835972/
+            match: ["*://*.toutiao.com/*"],
+            hide: ["a.j-expand-showfull.expand-bottom"],
+            expand: ["div.answer-text-full"],
+        },
         {
             match: ["*://www.bilibili.com/read/mobile?id=*"],
             hide: [".h5-download-bar", ".read-article-box .read-more .arrow-cnt"],
