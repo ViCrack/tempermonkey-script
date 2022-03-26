01d53589d63548bc14add6e4b2fd1182ee4cb6ad
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-03-26 15:31:10 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.62.0
+// @version     1.63.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十多个网站，前期脚本更新可能会比较频繁】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -73,6 +73,7 @@
 // @match       *://www.bilibili.com/read/mobile?id=*
 // @match       *://*.toutiao.com/*
 // @match       *://www.itbaoku.cn/*
+// @match       *://support.qq.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -81,6 +82,10 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://support.qq.com/*"],
+            directLink: ["*://support.qq.com/*/link-jump?jump=*", "jump"],
+        },
         {
             match: ["*://www.itbaoku.cn/*"],
             hide: [".qa-content-more"],
