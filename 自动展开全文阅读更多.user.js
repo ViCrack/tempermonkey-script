a0b43b5fe4492a694544ec87c0b4e441977eedcf
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-04-06 15:26:07 +0800

    add downxia

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.63.1
+// @version     1.64.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十多个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -74,6 +74,7 @@
 // @match       *://*.toutiao.com/*
 // @match       *://www.itbaoku.cn/*
 // @match       *://support.qq.com/*
+// @match       *://m.downxia.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -82,6 +83,16 @@
 
 (function () {
     var websites = [
+        {
+            match: "*://m.downxia.com/*",
+            hide: ["a.slide-btn"],
+            expand: [".chapter-cont"],
+            css: `
+                .chapter-cont.snbg {
+                    position: unset !important;
+                }
+            `,
+        },
         {
             match: ["*://support.qq.com/*"],
             directLink: ["*://support.qq.com/*/link-jump?jump=*", "jump"],
