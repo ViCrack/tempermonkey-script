27cc5210e5eb0121ad32977ce8c1fa849fbc4531
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-04-12 10:29:29 +0800

    up

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -2,7 +2,7 @@
 // @name        自动展开全文阅读更多
 // @version     1.65.0
 // @author      baster
-// @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十多个网站】
+// @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
 // @homepageURL https://greasyfork.org/zh-CN/users/306433
 // @namespace   https://greasyfork.org/zh-CN/users/306433
