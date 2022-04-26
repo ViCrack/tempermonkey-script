9640ea12511446d66c3a1f929f36b349d1e4f492
余剑衡 <yujh@techsure.com.cn>
2022-04-26 15:55:45 +0800

    add weibo

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.65.4
+// @version     1.66.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -76,6 +76,7 @@
 // @match       *://support.qq.com/*
 // @match       *://m.downxia.com/*
 // @match       *://m.sohu.com/a/*
+// @match       *://m.weibo.cn/u/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -84,6 +85,10 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://m.weibo.cn/u/*"],
+            directLink: ["*://weibo.cn/sinaurl?u=*", "u"],
+        },
         {
             // https://m.sohu.com/a/442909096_120459022/?pvid=000115_3w_a
             match: "*://m.sohu.com/a/*",
