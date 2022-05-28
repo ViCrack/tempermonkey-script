3b1bb2d61d03304c03619b774c2e1de9219a4ce1
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-05-28 17:04:09 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.73.1
+// @version     1.74.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -84,6 +84,7 @@
 // @match       *://m.yezicc.com/*
 // @match       *://*.ali213.net/*
 // @match       *://itpcb.com/*
+// @match       *://*.vovososo.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -92,6 +93,19 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://*.vovososo.com/*"],
+            directLink: [
+                "*/jump.html?url=*",
+                (node) => {
+                    let { url } = parseUrl(node.href);
+                    if (url) {
+                        node.href = atob(url);
+                        node.setAttribute("target", "_blank");
+                    }
+                },
+            ],
+        },
         {
             match: ["*://itpcb.com/*"],
             hide: ["#read-more-wrap"],
