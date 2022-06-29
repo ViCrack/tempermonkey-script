8e02fa564861aa5a26c58ff5104aa2f8c6cadd3b
余剑衡 <yujh@techsure.com.cn>
2022-06-29 10:09:27 +0800

    添加新网站

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.74.1
+// @version     1.75.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -86,6 +86,7 @@
 // @match       *://*.ali213.net/*
 // @match       *://itpcb.com/*
 // @match       *://*.vovososo.com/*
+// @match       *://www.lishixinzhi.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -111,6 +112,11 @@
             match: ["*://itpcb.com/*"],
             hide: ["#read-more-wrap"],
             expand: ["#container"],
+        },
+        {
+            match: ["*://www.lishixinzhi.com/*"],
+            hide: ["#showbtn"],
+            expand: ["#endtext"],
         },
         {
             match: ["*://*.ali213.net/*"],
