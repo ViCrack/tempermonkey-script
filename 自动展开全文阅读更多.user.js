3ca7f07ca417ddfdfd0dd73f5d1fa9d12cb14166
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-08-05 16:25:51 +0800

    修复百度知道无法展开全文

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.77.0
+// @version     1.77.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -614,9 +614,16 @@
             expand: [".txtContent"],
         },
         {
+            // https://zhidao.baidu.com/question/423275263.html
             match: "*://zhidao.baidu.com/question/*",
-            hide: [".w-detail-display-btn-text", ".wgt-best-mask"],
-            expand: [".w-detail-container.w-detail-index", "div[id^=best-content-]"],
+            hide: [".w-detail-display-btn-text", ".wgt-best-mask", ".wgt-answers-mask", ".wgt-question-desc-action"],
+            expand: [".w-detail-container.w-detail-index", "div[id^=best-content-]", "div[id^=answer-content-]", ".wgt-question-desc-inner"],
+            js: () => {
+                let e = document.getElementById("show-answer-hide");
+                if (e) {
+                    e.dispatchEvent(new Event("click"));
+                }
+            },
         },
         {
             match: "*://haokan.baidu.com/v*",
