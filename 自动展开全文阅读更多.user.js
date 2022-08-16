bf07dca065659e7afecdcabbaafbbb3dcc187439
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-08-16 16:30:28 +0800

    某些采集机翻引流站点

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.79.0
+// @version     1.80.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -91,6 +91,7 @@
 // @match       *://m.hexun.com/*
 // @match       *://www.javascriptcn.com/*
 // @match       *://*.zol.com.cn/*
+// @match       *://www.5axxw.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -103,6 +104,11 @@
             match: ["*://*.zol.com.cn/*"],
             hide: [".unfold-article-btn"],
             expand: [".article-content"],
+        },
+        {
+            // 某些采集机翻引流站点
+            match: ["*://www.5axxw.com/*"],
+            hide: ["#gzh-mask", "#gzh-modal"],
         },
         {
             match: ["*://m.hexun.com/*"],
