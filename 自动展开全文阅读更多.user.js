d9f9f2073c24812d63158254442b5fca0091e5b2
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-08-23 22:09:51 +0800

    修复知乎的末尾展开

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.80.0
+// @version     1.80.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -525,6 +525,15 @@
                 ],
             ],
             directLink: ["*link.zhihu.com/?target=*", "target"],
+            css: `
+                .RichContent--unescapable.is-collapsed .RichContent-inner {
+                    -webkit-mask-image: unset !important;
+                    mask-image: unset !important;
+                }
+                .RichContent.is-collapsed {
+                    cursor: unset !important;
+                }
+            `,
         },
         {
             match: "*://www.cnbeta.com/articles/*",
