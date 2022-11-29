5158266660b50fd3ef69fd18463323b1bbd92423
ViCrack <18179821+ViCrack@users.noreply.github.com>
2022-11-29 09:56:10 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.90.0
+// @version     1.91.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -102,6 +102,7 @@
 // @match       *://www.ngui.cc/*
 // @match       *://ddrv.cn/*
 // @match       *://www.qcc.com/*
+// @match       *://zj.cnr.cn/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -113,6 +114,11 @@
         {
             match: ["*://www.qcc.com/*"],
             directLink: ["*://www.qcc.com/web/transfer-link?link=*", "link"],
+        },
+        {
+            match: ["*://zj.cnr.cn/*"],
+            hide: ["#readmore"],
+            expand: ["#perny-main"],
         },
         {
             match: ["*://www.ngui.cc/*"],
