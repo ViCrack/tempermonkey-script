648f743118903929329fc318f6a4db4ec66b72cc
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-01-21 15:48:52 +0800

    优化腾讯新闻

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.92.0
+// @version     1.93.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -104,6 +104,7 @@
 // @match       *://www.qcc.com/*
 // @match       *://zj.cnr.cn/*
 // @match       *://*tinymind.net.cn/*
+// @match       *://view.inews.qq.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -133,6 +134,12 @@
                     configurable: false,
                 });
             },
+        },
+        {
+            // https://view.inews.qq.com/k/20210608A045BD00
+            match: ["*://view.inews.qq.com/*"],
+            hide: ["div[class^=show-more_outer]", "div[class^=slider-top-bar_sliderWrapper]"],
+            expand: ["div[class^=show-more_height-not-full]"],
         },
         {
             match: ["*://www.qcc.com/*"],
