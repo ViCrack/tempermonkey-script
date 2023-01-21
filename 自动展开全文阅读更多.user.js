f07749dd779ff5b81ab35d2f63236e25b62e0042
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-01-21 15:38:17 +0800

    添加新网站

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.91.2
+// @version     1.92.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -103,6 +103,7 @@
 // @match       *://ddrv.cn/*
 // @match       *://www.qcc.com/*
 // @match       *://zj.cnr.cn/*
+// @match       *://*tinymind.net.cn/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -111,6 +112,28 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://*tinymind.net.cn/*"],
+            start: () => {
+                // 或者废掉DOMMouseScroll
+                unsafeWindow.verify_flag = 1;
+                unsafeWindow.phone_verify_flag = 1;
+                Object.defineProperty(unsafeWindow, "verify_flag", {
+                    get: function () {
+                        return 1;
+                    },
+                    enumerable: true,
+                    configurable: false,
+                });
+                Object.defineProperty(unsafeWindow, "phone_verify_flag", {
+                    get: function () {
+                        return 1;
+                    },
+                    enumerable: true,
+                    configurable: false,
+                });
+            },
+        },
         {
             match: ["*://www.qcc.com/*"],
             directLink: ["*://www.qcc.com/web/transfer-link?link=*", "link"],
