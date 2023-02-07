164d4b1e9645279e89914fcad36524f7ca226cce
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-02-07 10:37:02 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.93.1
+// @version     1.94.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -93,11 +93,10 @@
 // @match       *://*.zol.com.cn/*
 // @match       *://www.5axxw.com/*
 // @match       *://3g.163.com/*
-// @match       *://www.pythonheidong.com/*
 // @match       *://code.js-code.com/*
 // @match       *://*wenda.so.com/*
 // @match       *://*.dxy.cn/*
-// @match       *://www.javaheidong.com/*
+// @match       *://*heidong.com/*
 // @match       *://www.tofacebook.com/*
 // @match       *://www.ngui.cc/*
 // @match       *://ddrv.cn/*
@@ -181,22 +180,7 @@
             expand: ["#codeContent"],
         },
         {
-            match: ["*://www.pythonheidong.com/*"],
-            hide: [".ggbox"],
-            start: () => {
-                // 阻止牛皮癣弹框
-                unsafeWindow.user_login = true;
-                Object.defineProperty(unsafeWindow, "user_login", {
-                    get: function () {
-                        return true;
-                    },
-                    enumerable: true,
-                    configurable: true,
-                });
-            },
-        },
-        {
-            match: ["*://www.javaheidong.com/*"],
+            match: ["*://*heidong.com/*"],
             hide: [".ggbox"],
             start: () => {
                 // 阻止牛皮癣弹框
