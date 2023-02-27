622c56a6153fa13f7fc073f3d777dbb2cf7fd611
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-02-27 10:26:10 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.94.1
+// @version     1.95.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -104,6 +104,7 @@
 // @match       *://*.cnr.cn/*
 // @match       *://*tinymind.net.cn/*
 // @match       *://view.inews.qq.com/*
+// @match       *://*gamersky.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -139,6 +140,12 @@
             match: ["*://view.inews.qq.com/*"],
             hide: ["div[class^=show-more_outer]", "div[class^=slider-top-bar_sliderWrapper]"],
             expand: ["div[class^=show-more_height-not-full]"],
+        },
+        {
+            // https://wap.gamersky.com/news/Content-1571244.html
+            match: ["*://*gamersky.com/*"],
+            hide: [".gsAreaContextOpen", ".ymwBootDownload"],
+            expand: ["#gsAreaContext"],
         },
         {
             match: ["*://www.qcc.com/*"],
