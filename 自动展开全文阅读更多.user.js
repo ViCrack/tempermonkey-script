215f07f1d33d98d4cd84e39f4808235997caac45
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-05-01 17:49:08 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.100.1
+// @version     1.101.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -113,6 +113,7 @@
 // @match       *://weibo.com/ttarticle/p/show*
 // @match       *://weibo.com/cmbchina*
 // @match       *://weibo.com/u/*
+// @match       *://www.ctfiot.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -121,6 +122,19 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://www.ctfiot.com/*"],
+            directLink: [
+                "*/go/?url=*",
+                (node) => {
+                    let { url } = parseUrl(node.href);
+                    if (url) {
+                        node.href = atob(url);
+                        node.setAttribute("target", "_blank");
+                    }
+                },
+            ],
+        },
         {
             // https://weibo.com/cmbchina
             match: ["*://weibo.com/cmbchina*", "*://weibo.com/u/*"],
