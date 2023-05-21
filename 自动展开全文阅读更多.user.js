e211af38523562f22927078edf728bb86a0ec8e4
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-05-21 19:36:43 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.101.1
+// @version     1.102.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -115,6 +115,7 @@
 // @match       *://weibo.com/cmbchina*
 // @match       *://weibo.com/u/*
 // @match       *://www.ctfiot.com/*
+// @match       *://www.autohotkey.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -123,6 +124,15 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://www.autohotkey.com/*"],
+            js: () => {
+                const items = document.querySelectorAll('.codebox > p > a:first-child + :nth-child(2)');
+                items.forEach(item => {
+                    expandCode(item);
+                });
+            },
+        },
         {
             match: ["*://www.ctfiot.com/*"],
             directLink: [
