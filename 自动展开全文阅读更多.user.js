cc47823f35fdc936efc208089e27f15ae2d9fb34
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-06-30 11:38:49 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.106.0
+// @version     1.107.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -120,6 +120,7 @@
 // @match       *://www.95pm.com/*
 // @match       *://*.betheme.net/*
 // @match       *://*.zoukankan.com/*
+// @match       *://*.hexun.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -136,6 +137,11 @@
                     location.href = nodeList[0].href;
                 }
             }
+        },
+        {
+            match: ["*://*.hexun.com/*"],
+            hide: ["div.showAll", "div.bottomLayer"],
+            expand: ["div.art_contextBox"],
         },
         {
             match: ["*://*.betheme.net/*"],
