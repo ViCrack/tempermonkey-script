bed7a3bb809d6451dccf09b5999e339cafbdcdaa
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-07-11 17:02:36 +0800

    添加一个新网站

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.109.1
+// @version     1.110.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -123,6 +123,7 @@
 // @match       *://*.hexun.com/*
 // @match       *://*.xjx100.cn/*
 // @match       *://*.yii666.com/*
+// @match       *://*.pianshen.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -131,6 +132,17 @@
 
 (function () {
     var websites = [
+        {
+            // 抄袭站直接跳转到原文即可
+            match: ["*://*.pianshen.com/*"],
+            js: () => {
+                $('head').append('<meta name="referrer" content="never">');
+                let url = $('#gotosource a').attr('href');
+                if (url) {
+                    location.href = url;
+                }
+            }
+        },
         {
             // 抄袭站直接跳转到原文即可
             match: ["*://*.yii666.com/*"],
