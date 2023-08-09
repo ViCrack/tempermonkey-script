2bba5146eb6ec95cca1c75337163d6e28f028c01
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-08-09 11:42:55 +0800

    环球网修复

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.113.0
+// @version     1.113.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -10,7 +10,7 @@
 // @match       *://m.36kr.com/*
 // @match       *://ext.baidu.com/api/comment/*
 // @match       *://www.xz577.com/*
-// @match       *://m.huanqiu.com/*
+// @match       *://*.huanqiu.com/*
 // @match       *://iknow.lenovo.com.cn/*
 // @match       *://g.pconline.com.cn/*
 // @match       *://www.it1352.com/*
@@ -964,7 +964,12 @@
             expand: [".art-content"],
         },
         {
-            match: "*://m.huanqiu.com/*",
+            match: "*://3w.huanqiu.com/*",
+            hide: ["#more"],
+            expand: ["#content"],
+        },
+        {
+            match: ["*://m.huanqiu.com/*"],
             hide: [".unfold-btn"],
             expand: [".article-content", ".article .content"],
             start: () => {
