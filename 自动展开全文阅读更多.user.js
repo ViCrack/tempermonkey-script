10e7e608b0de3d8f7134ce24054c69b4ada03206
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-10-13 17:32:00 +0800

    theqoo 自动展开

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.115.0
+// @version     1.116.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
 // @homepageURL https://greasyfork.org/zh-CN/users/306433
 // @namespace   https://greasyfork.org/zh-CN/users/306433
 // @icon        https://img.icons8.com/stickers/100/000000/double-down.png
+// @match       *://theqoo.net/*
 // @match       *://m.36kr.com/*
 // @match       *://ext.baidu.com/api/comment/*
 // @match       *://www.xz577.com/*
@@ -138,6 +139,20 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://theqoo.net/*"],
+            js: () => {
+                jQuery(function () {
+                    $(document).ajaxComplete(function (event, xhr, settings) {
+                        if (settings && settings.data && settings.data.includes('act=dispBoardContentCommentListTheqoo')) {
+                            if ($(".show_more:not([style='display: none;'])").length > 0) {
+                                $(".show_more:not([style='display: none;'])").click();
+                            }
+                        }
+                    });
+                });
+            }
+        },
         {
             match: ["*://m.freebuf.com/*"],
             js: () => {
