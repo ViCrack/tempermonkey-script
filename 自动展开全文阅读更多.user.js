2a3dd26bec622af8352b809b9b00ddff1c152306
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-11-14 14:12:06 +0800

    add

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.116.1
+// @version     1.117.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -131,6 +131,7 @@
 // @match       *://www.nuomiphp.com/*
 // @match       *://dhexx.cn/*
 // @match       *://*.exyb.cn/*
+// @match       *://ispacesoft.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -139,6 +140,11 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://ispacesoft.com/*"],
+            hide: [".entry-readmore"],
+            expand: [".entry-content"],
+        },
         {
             // https://theqoo.net/hot/2963319501
             match: ["*://theqoo.net/*"],
