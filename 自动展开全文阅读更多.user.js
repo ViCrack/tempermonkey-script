61dfd506c41cfb0203a6e20c522b9632664b53e4
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-11-21 18:09:41 +0800

    修复网站代码更新导致规则失效

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.117.0
+// @version     1.117.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -151,7 +151,7 @@
             js: () => {
                 var nowScroll = $(window).scrollTop();
                 $(document).ajaxComplete(function (event, xhr, settings) {
-                    if (settings && settings.data && settings.data.includes('act=dispBoardContentCommentListTheqoo')) {
+                    if (settings && settings.data && (settings.data.includes('act=dispBoardContentCommentListTheqoo') || settings.data.includes('act=dispTheqooContentCommentListTheqoo'))) {
                         $(window).scrollTop(nowScroll);
                         if ($(".show_more:not([style='display: none;'])").length > 0) {
                             $(".show_more:not([style='display: none;'])").click();
