41b1ec79e6859de772699fde916ac38a5a88ee11
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-12-15 10:20:25 +0800

    txcqq链接直接跳转

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.119.0
+// @version     1.120.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -134,6 +134,7 @@
 // @match       *://ispacesoft.com/*
 // @match       *://www.3424.cn/*
 // @match       *://*.cnq.net/*
+// @match       *://txc.qq.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -142,6 +143,10 @@
 
 (function () {
     var websites = [
+        {
+            match: "*://txc.qq.com/*",
+            directLink: ["*/link-jump?jump=*", "jump"],
+        },
         {
             match: ["*://*.cnq.net/*"],
             hide: ["#readmore-wrapper"],
