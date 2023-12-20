5425855c9953a38928252dabb54f1406354b47b9
ViCrack <18179821+ViCrack@users.noreply.github.com>
2023-12-20 13:42:44 +0800

    add wenku.csdn

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.121.1
+// @version     1.121.2
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -16,6 +16,7 @@
 // @match       *://g.pconline.com.cn/*
 // @match       *://www.it1352.com/*
 // @match       *://www.jianshu.com/p/*
+// @match       *://wenku.csdn.net/*
 // @match       *://blog.csdn.net/*
 // @match       *://download.csdn.net/download/*
 // @match       *://jingyan.baidu.com/article/*
@@ -1149,6 +1150,11 @@
                 btn.click()
                 $("#desc-text").prop("checked", true);
             }
+        },
+        {
+            match: ["*://wenku.csdn.net/*"],
+            hide: [".article-box .cont .open", ".article-box .vip"],
+            expand: [".article-box .cont.first-show"],
         },
         {
             match: "*://blog.csdn.net/*",
