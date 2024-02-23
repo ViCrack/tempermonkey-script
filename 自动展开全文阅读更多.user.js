3e5a60b0c20702f535d3f85e4c3650da3d17a0fe
ViCrack <18179821+ViCrack@users.noreply.github.com>
2024-02-23 10:05:21 +0800

    添加新网站

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.129.0
+// @version     1.130.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -143,6 +143,7 @@
 // @match       *://*.chinaacc.com/*
 // @match       *://lvlin.baidu.com/*
 // @match       *://*.64365.com/*
+// @match       *://programmercarl.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -151,6 +152,11 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://programmercarl.com/*"],
+            hide: ["#read-more-wrap"],
+            expand: [".theme-default-content"],
+        },
         {
             match: ["*://*.64365.com/*"],
             hide: [".zs-unfold .unfd-btn-bar"],
