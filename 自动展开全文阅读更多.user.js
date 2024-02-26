2ac2509e7bf18a704c8e8e906fad357a3f12a8c5
ViCrack <18179821+ViCrack@users.noreply.github.com>
2024-02-26 13:51:00 +0800

    优化bilibili手机端

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.130.0
+// @version     1.130.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -71,7 +71,7 @@
 // @match       *://cn.bing.com/search?*
 // @match       *://segmentfault.com/*
 // @match       *://ld246.com/*
-// @match       *://www.bilibili.com/*
+// @match       *://*.bilibili.com/*
 // @match       *://*.toutiao.com/*
 // @match       *://www.itbaoku.cn/*
 // @match       *://support.qq.com/*
@@ -655,10 +655,13 @@
                 }
             },
         },
-        // {   去掉这个功能，目前看起来用处不大
-        //     match: ["*://m.bilibili.com/video/*"],
-        //     hide: [".launch-app-btn"],
-        // },
+        {
+            match: ["*://m.bilibili.com/video/*"],
+            hide: [".launch-app-btn.m-nav-openapp"],
+            wait: [
+                ["div.to-see", "click"]
+            ]
+        },
         {
             match: ["*://ld246.com/*"],
             directLink: ["*://*ld246.com/forward?goto=*", "goto"],
@@ -1547,7 +1550,7 @@
                     } catch (x) {
                         clearInterval(id);
                     }
-                }, 2000);
+                }, 1000);
             }
 
             if ("directLink" in website) {
