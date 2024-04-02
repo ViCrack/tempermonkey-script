7eaa8b65f6cd74643224b40b81faec977c6356ff
ViCrack <18179821+ViCrack@users.noreply.github.com>
2024-04-02 11:22:05 +0800

    新增适配两个网站

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.132.2
+// @version     1.134.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -147,6 +147,8 @@
 // @match       *://programmercarl.com/*
 // @match       *://*.qdxiaochuan.com/*
 // @match       *://*.zjh336.cn/*
+// @match       *://*.volcengine.com/*
+// @match       *://*.289.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -155,6 +157,16 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://*.289.com/*"],
+            hide: [".f-show-cont"],
+            expand: [".m-downmain"],
+        },
+        {
+            match: ["*://*.volcengine.com/*"],
+            hide: ["div[class*='button-AVOE'][class*='volcfe-flex-middle']", "div[class^='cover-']"],
+            expand: ["div[class^='content-'][class*='hidden-']"],
+        },
         {
             match: ["*://*.zjh336.cn/*"],
             hide: [".readnum"],
