c6c816abf05d26f67b0c0291714874ef28578158
ViCrack <18179821+ViCrack@users.noreply.github.com>
2024-04-07 17:42:08 +0800

    添加新网址

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.134.0
+// @version     1.135.0
 // @author      baster
-// @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持几十个网站】
+// @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
 // @homepageURL https://greasyfork.org/zh-CN/users/306433
 // @namespace   https://greasyfork.org/zh-CN/users/306433
@@ -149,6 +149,7 @@
 // @match       *://*.zjh336.cn/*
 // @match       *://*.volcengine.com/*
 // @match       *://*.289.com/*
+// @match       *://*.hi-linux.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -157,6 +158,11 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://*.hi-linux.com/*"],
+            hide: ["#read-more-wrap"],
+            expand: ["#vip-container"],
+        },
         {
             match: ["*://*.289.com/*"],
             hide: [".f-show-cont"],
