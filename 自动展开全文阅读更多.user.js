4b9049150d94807073f1819e6075e5614fed00a0
ViCrack <18179821+ViCrack@users.noreply.github.com>
2024-06-20 10:36:37 +0800

    增加新网站支持

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.143.1
+// @version     1.144.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -36,6 +36,7 @@
 // @match       *://xuedingmiao.com/*
 // @match       *://xw.qq.com/*
 // @match       *://mbd.baidu.com/newspage/*
+// @match       *://ailegal.baidu.com/*
 // @match       *://www.cnbeta.com/articles/*
 // @match       *://www.zhihu.com/question/*
 // @match       *://zhuanlan.zhihu.com/p/*
@@ -161,6 +162,7 @@
 // @match       *://discussions*.apple.com/*
 // @match       *://www.msn.cn/*
 // @match       *://*.pcbaby.com.cn/*
+// @match       *://*.youzan.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -206,6 +208,16 @@
                     return shadow;
                 };
             },
+        },
+        {
+            match: ["*://*.youzan.com/*"],
+            hide: [".cms-article-lock"],
+            expand: [".content-content"],
+        },
+        {
+            match: ["*://ailegal.baidu.com/*"],
+            hide: [".article-content .expand", ".article-content .mask"],
+            expand: [".article-content .article-text"],
         },
         {
             match: ["*://*.pcbaby.com.cn/*"],
