554856472767628a43945f2ed67ace7d92c3831a
ViCrack <18179821+ViCrack@users.noreply.github.com>
2024-08-02 10:15:14 +0800

    添加几个新网址

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.146.0
+// @version     1.148.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -165,6 +165,8 @@
 // @match       *://*.youzan.com/*
 // @match       *://*.93300.cn/*
 // @match       *://sspai.com/*
+// @match       *://*.xjishu.com/*
+// @match       *://*.eastday.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -173,6 +175,16 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://*.eastday.com/*"],
+            hide: [".expand-btn"],
+            expand: [".post-content"],
+        },
+        {
+            match: ["*://*.xjishu.com/*"],
+            hide: [".gradBox", ".art-download"],
+            expand: [".con-box"],
+        },
         {
             match: ["*://sspai.com/*"],
             hide: ["#btn-call-app"],
@@ -702,8 +714,9 @@
         {
             // https://wenda.so.com/q/1679797307210735
             match: ["*://*wenda.so.com/*"],
-            hide: ["#show-rest-entry", ".answer-part__has-folder__btn"],
+            hide: ["#show-rest-entry", ".answer-part__has-folder__btn", ".unfold-page.js-unfold-page"],
             expand: ["#det-content", ".answer-part__has-folder"],
+            wait: [[".hide.js-unfold-answer.answer-fold-box", (node) => node.classList.remove("hide")]],
         },
         {
             match: ["*://*.js-code.com/*"],
