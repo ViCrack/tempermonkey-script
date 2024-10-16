3e60cd8c7ea0fc66704da587f9a7f6212930888b
ViCrack <18179821+ViCrack@users.noreply.github.com>
2024-10-16 11:03:24 +0800

    去掉搜狐网app内打开的悬浮窗

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.152.0
+// @version     1.152.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -860,7 +860,7 @@
         {
             // https://m.sohu.com/a/442909096_120459022/?pvid=000115_3w_a
             match: "*://*.sohu.com/*",
-            hide: [".lookall-box"],
+            hide: [".lookall-box", "#CallAppContainer"],
             css: `
                 .hidden-content.hide,.control-hide {
                     display: block !important;
