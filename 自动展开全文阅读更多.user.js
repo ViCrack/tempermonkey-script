b1262a02da43bf2595aff8ff4709634fc7113858
ViCrack <18179821+ViCrack@users.noreply.github.com>
2025-01-06 15:28:59 +0800

    feat: 更新版本并添加新网站支持

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.154.1
+// @version     1.155.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -176,6 +176,8 @@
 // @match       *://*.zmtpc.com/*
 // @match       *://m.eeo.com.cn/*
 // @match       *://*.sina.cn/*
+// @match       *://jpom.top/*
+// @match       *://*.xlxbk.cn/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -184,6 +186,17 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://*.xlxbk.cn/*"],
+            hide: [".read-more"],
+            expand: ["[data-nav='posts']"],
+        },
+        {
+            match: ["*://jpom.top/*"],
+            js: () => {
+                unsafeWindow.localStorage.isStarJpomRepo = new Date().getTime();
+            },
+        },
         {
             match: ["*://*.sina.cn/*"],
             hide: [".read-unfold-box", "#article_end + div", "#m-sentiment3"],
