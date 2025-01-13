bda8f024735621d6e3d0f727c83b49926a1daafd
ViCrack <18179821+ViCrack@users.noreply.github.com>
2025-01-13 14:44:23 +0800

    feat(toutiao): 优化处理今日头条的折叠内容

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.156.0
+// @version     1.157.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -380,8 +380,8 @@
         },
         {
             match: ["*://*.youlai.cn/*"],
-            hide: [".mip-showore-btn-bar"],
-            expand: ["#show-more"],
+            hide: [".mip-showore-btn-bar", "div[class*='showBtn']"],
+            expand: ["#show-more", "#js-content"],
         },
         {
             match: ["*://*.gaodun.com/*"],
@@ -959,8 +959,8 @@
         {
             // https://wukong.toutiao.com/question/6712757183118835972/
             match: ["*://*.toutiao.com/*"],
-            hide: ["a.j-expand-showfull.expand-bottom", ".m-share-answer .neck .mask", ".float-openapp", ".expand-container .expand-button-wrapper", ".wenda-answer-content .expand-button-wrapper"],
-            expand: ["div.answer-text-full", "article.content", ".expand-container.folded", ".wenda-answer-content"],
+            hide: ["a.j-expand-showfull.expand-bottom", ".m-share-answer .neck .mask", ".float-openapp", ".expand-container .expand-button-wrapper", ".wenda-answer-content .expand-button-wrapper", ".fold-outer-container .fold-button", ".fold-outer-container .fold-mask", ".arco-show-monitor .m-bottom-container"],
+            expand: ["div.answer-text-full", "article.content", ".expand-container.folded", ".wenda-answer-content", ".fold-outer-container .fold-container"],
         },
         {
             match: ["*://www.bilibili.com/*"],
