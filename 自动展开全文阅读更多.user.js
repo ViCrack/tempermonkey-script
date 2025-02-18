9942997518ec8e13dbbd3297fabc37a0b0dec29e
ViCrack <18179821+ViCrack@users.noreply.github.com>
2025-02-18 14:15:49 +0800

    添加pixiv支持

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.162.0
+// @version     1.163.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -185,6 +185,7 @@
 // @match       *://*.528045.com/*
 // @match       *://*.xilichi.com/*
 // @match       *://*.ghxi.com/*
+// @match       *://*.pixiv.net/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -193,6 +194,14 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://*.pixiv.net/*"],
+            wait: [
+                [
+                    "div:contains('浏览更多')", "click"
+                ]
+            ]
+        },
         {
             match: ["*://*.ghxi.com/*"],
             js: () => {
