6642813bc02684f3125c4d4777972b81a055ef9d
ViCrack <18179821+ViCrack@users.noreply.github.com>
2025-02-22 13:10:43 +0800

    适配懂车帝

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.163.0
+// @version     1.164.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -186,6 +186,7 @@
 // @match       *://*.xilichi.com/*
 // @match       *://*.ghxi.com/*
 // @match       *://*.pixiv.net/*
+// @match       *://*.dongchedi.com/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -230,6 +231,31 @@
                     }
                 ]
             ]
+        },
+        {
+            match: ["*://*.dongchedi.com/*"],
+            js: () => {
+                let node = document.querySelector("article[class*='index_article_']");
+                if (node) {
+                    node.parentNode.style.height = "auto";
+                    node.parentNode.style.overflow = "auto";
+                }
+                node = document.querySelector("div[style='width:84px;height:38px;background:#ffcc32']");
+                if (node) {
+                    if (node.innerText.includes('打开App')) {
+                        node.parentNode.parentNode.parentNode.remove();
+                    }
+                }
+                node = document.querySelector("article > [class^='index_folder_']");
+                if (node) {
+                    node.remove();
+                }
+                let nodes = querySelectorIncludesText('.tw-w-full.tw-flex.tw-text-16.tw-items-center.tw-justify-center', '打开懂车帝APP，阅读完整内容');
+                nodes.forEach(item => {
+                    item.remove();
+                });
+
+            },
         },
         {
             match: ["*://*.xilichi.com/*"],
