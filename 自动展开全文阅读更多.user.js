1560a67ace13da09cfda7fd31e0ed9927b5faea4
ViCrack <18179821+ViCrack@users.noreply.github.com>
2025-05-01 22:43:17 +0800

    ✨ feat(sspai): 为少数派网站添加直接链接支持

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.171.0
+// @version     1.171.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -395,6 +395,7 @@
         {
             match: ["*://sspai.com/*"],
             hide: ["#btn-call-app"],
+            directLink: ["*/link?target=*", "target"],
         },
         {
             match: ["*://easylearn.baidu.com/edu-page/tiangong/*"],
