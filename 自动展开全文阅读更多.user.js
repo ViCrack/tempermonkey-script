3e57d35ff29535105a16b2bdcc893cc05a64470e
ViCrack <18179821+ViCrack@users.noreply.github.com>
2025-06-20 09:45:32 +0800

    ✨ feat(自动展开全文): 更新版本至1.174.0并新增对掘金网站的广告处理

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.173.1
+// @version     1.174.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -1383,6 +1383,12 @@
         {
             match: "*://juejin.cn/post/*",
             directLink: ["*link.juejin.cn/?target=*", "target"],
+            hide: [".top-banners-container > .global-top-banner"],
+            css: `
+                .header-with-banner {
+                    top: 0px !important;
+                }
+            `
         },
         {
             match: "*://www.3h3.com/soft/*",
