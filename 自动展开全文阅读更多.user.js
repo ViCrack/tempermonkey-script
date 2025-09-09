01344325118701a860bc34cdc066bfc8a4fb4927
ViCrack <18179821+ViCrack@users.noreply.github.com>
2025-09-09 21:00:45 +0800

    ✨ feat(自动展开全文阅读更多): 更新版本至1.175.1并新增对腾讯云博客文章的直接链接支持

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.175.0
+// @version     1.175.1
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -1302,6 +1302,7 @@
             match: "*://cloud.tencent.com/*",
             hide: [".cdc-expand-area__toggle", ".com-markdown-collpase-hide .com-markdown-collpase-toggle", ".J-headerAdvertising", ".c-nav-advertising"],
             expand: [".cdc-expand-area__main", ".com-markdown-collpase-hide .com-markdown-collpase-main"],
+            directLink: ["*://cloud.tencent.com/developer/tools/blog-entry?target=*", "target"],
         },
         {
             // https://developer.aliyun.com/article/875173
