57bd52d5aa7d5782104f6d85f2299c14c6dc9ae8
ViCrack <18179821+ViCrack@users.noreply.github.com>
2026-01-16 15:44:14 +0800

    ✨ feat(自动展开全文阅读更多): 更新版本至1.179.0并新增对linux.do的支持

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.178.0
+// @version     1.179.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -200,6 +200,7 @@
 // @match       *://*.zaobao.com/*
 // @match       *://*.kdocs.cn/*
 // @match       *://m.hexun.com/*
+// @match       *://linux.do/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -208,6 +209,24 @@
 
 (function () {
     var websites = [
+        {
+            match: ["*://linux.do/*"],
+            hookEvent: function (type, func, useCapture) {
+                if (type == "click" || type == "auxclick" || type == "contextmenu") {
+                    if (this.tagName === 'A' &&
+                        this.href &&
+                        (this.classList.contains('normal-external-link-icon') ||
+                            this.classList.contains('danger-external-link-icon') ||
+                            this.classList.contains('risky-external-link-icon') ||
+                            this.classList.contains('blocked-links'))) {
+
+                        this.setAttribute("target", "_blank");
+                        return true;
+                    }
+                }
+                return false;
+            },
+        },
         {
             match: ["*://m.hexun.com/*"],
             hide: [".article-open", "#download-app", "#info-banner"],
@@ -1186,7 +1205,7 @@
             match: ["*://*.toutiao.com/*"],
             hide: ["a.j-expand-showfull.expand-bottom", ".m-share-answer .neck .mask", ".float-openapp", ".expand-container .expand-button-wrapper", ".wenda-answer-content .expand-button-wrapper", ".fold-outer-container .fold-button", ".fold-outer-container .fold-mask", ".arco-show-monitor .m-bottom-container", ".fold-content > .arco-show-monitor", ".content.collapsed > .content-shadow", ".m-top-container.m-top-larger.m-top-no-search.news_article", ".m-top-padding", ".expand-container .expand-button-wrapper"],
             expand: ["div.answer-text-full", "article.content", ".expand-container.folded", ".wenda-answer-content", ".fold-outer-container .fold-container", ".content.collapsed"],
-            hookEvent: (type, func, useCapture) => {
+            hookEvent: function (type, func, useCapture) {
                 if (type == "click") {
                     console.log(this);
                     console.log("hookEvent", type, func, useCapture);
