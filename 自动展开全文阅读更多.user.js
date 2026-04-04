aac38689c88d54afb061ead76725d3396ec07772
ViCrack <18179821+ViCrack@users.noreply.github.com>
2026-04-04 22:10:52 +0800

    ✨ feat(自动展开全文阅读更多): 更新版本至1.182.0

--- 自动展开全文阅读更多.user.js
+++ 自动展开全文阅读更多.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动展开全文阅读更多
-// @version     1.181.1
+// @version     1.182.0
 // @author      baster
 // @description 自动展开网站全文内容而无需点击，去掉一些烦人广告，去掉需要打开app的提示，站外链直达(支持鼠标左右键和拖拽打开)，避免网址重定向浪费时间，支持免登陆复制文字，兼容手机和电脑端。 -- 【目前已支持上百个网站】
 // @supportURL  https://greasyfork.org/zh-CN/users/306433
@@ -200,6 +200,7 @@
 // @match       *://linux.do/*
 // @match       *://*.gitcode.com/*
 // @match       *://security.feishu.cn/*
+// @match       *://zxki.cn/*
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       unsafeWindow
@@ -222,6 +223,20 @@
             match: ["*://*.gitcode.com/*"],
             hide: [".blog-content-detail-mask", ".login-tip"],
             expand: [".blog-content-detail"],
+        },
+        {
+            match: ["*://zxki.cn/*"],
+            start: () => {
+                document.addEventListener('click', function (e) {
+                    const downloadBtn = e.target.closest('.download_item_btn');
+                    if (downloadBtn) {
+                        e.stopImmediatePropagation();
+                        downloadBtn.setAttribute("target", "_blank");
+                        downloadBtn.setAttribute("rel", "noreferrer");
+                        // window.open(downloadBtn.getAttribute('href'), '_blank', 'noreferrer');
+                    }
+                }, true);
+            },
         },
         {
             match: ["*://linux.do/*"],
@@ -1994,7 +2009,7 @@
                                 jQuery("code").attr("onclick", "mdcp.copyCode(event)");
                                 try {
                                     unsafeWindow.csdn.copyright.init("", "", "");
-                                } catch (err) {}
+                                } catch (err) { }
                                 try {
                                     Object.defineProperty(unsafeWindow.csdn.report, "reportClick", {
                                         value: function () {
@@ -2003,14 +2018,14 @@
                                         writable: false,
                                         configurable: false,
                                     });
-                                } catch (err) {}
+                                } catch (err) { }
                                 try {
                                     Object.defineProperty(unsafeWindow, "articleType", {
                                         value: 0,
                                         writable: false,
                                         configurable: false,
                                     });
-                                } catch (err) {}
+                                } catch (err) { }
                             }
                         });
                     });
